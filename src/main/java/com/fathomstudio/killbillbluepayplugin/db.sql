12e65bc13599448dc7e4ce59bc688e22b5410500
chris13524 <chris@hichris.com>
2017-08-07 16:08:52 -0400

    Fixed issue where ON DUPLICATE KEY UPDATE would not detect duplicate keys because of the lack of a UNIQUE column.

--- src/main/java/com/fathomstudio/killbillbluepayplugin/db.sql
+++ src/main/java/com/fathomstudio/killbillbluepayplugin/db.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS `bluePay_paymentMethods`;
 CREATE TABLE `bluePay_paymentMethods` (
-  `id`              INT(11)      NOT NULL AUTO_INCREMENT,
-  `paymentMethodId` VARCHAR(255) NOT NULL,
+  `id`              INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `paymentMethodId` VARCHAR(255) NOT NULL UNIQUE,
   `transactionId`   VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
   INDEX `INDEX_bluePay_paymentMethods_ON_paymentMethodId`(`paymentMethodId`)
 )
   ENGINE = InnoDB
@@ -12,12 +11,11 @@ CREATE TABLE `bluePay_paymentMethods` (
 
 DROP TABLE IF EXISTS `bluePay_credentials`;
 CREATE TABLE `bluePay_credentials` (
-  `id`        INT(11)      NOT NULL AUTO_INCREMENT,
-  `tenantId`  VARCHAR(255) NOT NULL,
+  `id`        INT(11)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `tenantId`  VARCHAR(255) NOT NULL UNIQUE,
   `accountId` VARCHAR(255),
   `secretKey` VARCHAR(255),
-  `test`   BOOLEAN,
-  PRIMARY KEY (`id`),
+  `test`      BOOLEAN,
   INDEX `INDEX_bluePay_credentials_ON_tenantId`(`tenantId`),
   INDEX `INDEX_bluePay_credentials_ON_accountId`(`accountId`)
 )
